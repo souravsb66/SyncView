ad678e14fca3c3c389fe61ea9cf877159f8a313d
Sourav Biswas <sourav.sb56@gmail.com>
2024-01-09 14:25:52 +0530

    Frontend Innitial Setup

---
+++ frontend/src/routes/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+  )
+}
+
+export default AllRoutes
